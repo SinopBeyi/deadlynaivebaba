f6ff235047737eceeb4b99258a8783ff8774fdbe
SinopBeyi <79184900+SinopBeyi@users.noreply.github.com>
2024-09-08 09:02:02 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  TOKEN: "", //write your discord bot token
+  url: "1881", //Type the Custom Discord Server URL You Want to Retrieve!
+  serverID: "1251408792370024568", //Type the Server ID to Get the Url!
+
+  auth:{
+    discordAccountToken: "330029908447199232" //Type a discord account token that is authorised on the discord server where you will receive the url.
+  }
+}
+
+module.exports = config
---
+++ index.js
@@ -0,0 +1,50 @@
+const {
+Client,
+GatewayIntentBits,
+Partials,
+} = require("discord.js");
+const fs = require("fs");
+const mySecret = process.env['TOKEN']
+const config = require("./config.js");
+const client = new Client({
+partials: [
+Partials.Message, // for message
+Partials.Channel, // for text channel
+Partials.GuildMember, // for guild member
+],
+intents: [
+GatewayIntentBits.Guilds, // for guild related things
+GatewayIntentBits.GuildMembers, // for guild members related things
+GatewayIntentBits.GuildMessages, // for guild messages things
+GatewayIntentBits.MessageContent // enable if you need message content things
+],
+});
+
+module.exports = client;
+
+fs.readdir("./events", (_err, files) => {
+files.forEach((file) => {
+if (!file.endsWith(".js")) return;
+const event = require(`./events/${file}`);
+let eventName = file.split(".")[0];
+console.log(`👌 Loadded Event: ${eventName}`);
+client.on(eventName, event.bind(null, client));
+delete require.cache[require.resolve(`./events/${file}`)];
+});
+});
+
+
+client.login(config.TOKEN || process.env.TOKEN).catch(e => {
+console.log("Your Bot Token is Invalid Or Your Bot's INTENTS Are OFF! Please visit https://blog.codeshare.me/2023/12/discord-vanity-url-sniper-usage.html and check your bot's token and intents.")
+})
+
+const express = require('express');
+const app = express();
+const port = 3100;//buraya karışmayın.
+
+app.get('/', (req, res) => res.send('we discord'));//değiştirebilirsiniz.
+
+app.listen(port, () =>
+console.log(`Bot bu adres üzerinde çalışıyor: http://localhost:${port}`)//port
+);
+app.listen(process.env.PORT);
